02bd0206bd10abf424803db4f87e9a4e5992b0ae
code-tieumomo <code.tieumomo@gmail.com>
2023-10-04 18:11:07 +0700

    fix: wrong command

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Mail\OrderShipped;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Mail::to('minhthao9968@gmail.com')->send(new OrderShipped());
 });
